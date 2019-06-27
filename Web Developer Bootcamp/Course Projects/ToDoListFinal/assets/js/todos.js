b1cbec0c324e4218632e828c206d8a995864a6e0
Malika Navruzova <n.malika1999@gmail.com>
2019-06-27 12:42:47 -0400

    Add Paper.js project

--- Web Developer Bootcamp/Course Projects/ToDoListFinal/assets/js/todos.js
+++ Web Developer Bootcamp/Course Projects/ToDoListFinal/assets/js/todos.js
@@ -1,1 +1,41 @@
-alert("CONNECTED");
+//Check off specific ToDos by Clicking 
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//***** ALTERNATIVE ******
+// //If li is gray 
+// 	if ($(this).css("color")==="rgb(128, 128, 128)") {
+// 		//we turn it black
+// 		$(this).css ({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+// 	} else {
+// 		$(this).css ({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove(); //"this" refers to the li, not the span 
+	});
+	event.stopPropagation(); //THIS WILL STOP A PHENOMENON CALLED EVENT BUBBLING
+});
+
+$("input[type='text'").keypress(function(event){
+	if ((event).which===13) {
+		//grab new todo text from input;
+		var todoText =$(this).val(); 
+		$(this).val("");
+		//create a new li and add to ul 
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
