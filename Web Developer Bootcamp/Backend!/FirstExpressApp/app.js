95654aeecedff72a6dec0cb1f352bf7fff2f93f0
Malika Navruzova <n.malika1999@gmail.com>
2019-07-09 16:28:28 -0400

    Add custom assets lesson

--- Web Developer Bootcamp/Backend!/FirstExpressApp/app.js
+++ Web Developer Bootcamp/Backend!/FirstExpressApp/app.js
@@ -1,3 +1,29 @@
-console.log("OUR EXPRESS APP WILL GO HERE");
+
 var express = require("express");
 var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res){
+	res.send("Hi there!");
+});
+// "/bye" => "Goodbye"
+app.get("/bye", function(req, res){
+	res.send("Goodbye");
+});
+// "/dog" => "Meow"
+app.get("/dog", function(req, res){
+	res.send("Meow");
+});
+
+app.get("/r/:subredditName", function(req, res){
+	var subreddit = req.params.subredditName;
+	res.send("WELCOME TO THE" + subreddit.to + "SUBREDDIT")
+});
+
+app.get("*", function(req,res){
+	res.send("You are a star");
+});
+
+app.listen(3000, function(){
+	console.log("Server has started");
+});
