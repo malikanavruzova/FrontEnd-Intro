9b78b08bac6cb17965e3d2b70736bfef88a71789
Malika Navruzova <n.malika1999@gmail.com>
2019-07-09 20:00:12 -0400

    Add post requests lesson

--- Web Developer Bootcamp/Backend!/FirstExpressApp/PostRequestDemo/app.js
+++ Web Developer Bootcamp/Backend!/FirstExpressApp/PostRequestDemo/app.js
@@ -1,14 +1,21 @@
 var express = require ("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+var friends = ["Malika", "Rocket", "Nobody", "Nobody"]
 
 app.get("/", function(req, res){
 	res.render("home");
 });
+app.post("/addfriend", function(req, res){
+	var newFriend = req.body.newfriend;
+	friends.push(newFriend);
+	res.redirect("/friends");
+})
 app.get("/friends", function(req, res){
-	var friends = ["Malika", "Rocket", "Nobody", "Nobody"]
-	res.render("friends");
+	res.render("friends", {friends: friends});
 });
 
 
