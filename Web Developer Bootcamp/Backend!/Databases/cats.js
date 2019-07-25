e165bb8de80e3f2d0b7d97ae73b9bee16fb3b840
Malika Navruzova <n.malika1999@gmail.com>
2019-07-25 17:55:07 -0400

    Add node modules

---
+++ Web Developer Bootcamp/Backend!/Databases/cats.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/cat_app', { useNewUrlParser: true });
+
+var catSchema = new mongoose.Schema({
+	name: String,
+	age: Number,
+	temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+//add a new cat to the database 
+var Georgy = new Cat({
+	name: "Georgy", 
+	age: 11,
+	temperament: "Grouchy"
+});
+
+george.save();
+//retrieve all cats from the DB and console.log each one 
