f52cfe24ddba5ff51994b2ca595d345cd00d8bdc
Malika Navruzova <n.malika1999@gmail.com>
2019-07-29 15:44:41 -0400

    Initialize exercises

--- React/contacts/src/App.js
+++ React/contacts/src/App.js
@@ -6,47 +6,28 @@ const contacts = [
    "id": "karen",
    "name": "Karen Isgrigg",
    "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
+   "avatarURL": "http://localhost:5001/public/karen.jpg"
  },
  {
    "id": "richard",
    "name": "Richard Kalehoff",
    "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
+   "avatarURL": "http://localhost:5001/public/richard.jpg"
  },
  {
    "id": "tyler",
    "name": "Tyler McGinnis",
    "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
+   "avatarURL": "http://localhost:5001/public/tyler.jpg"
  }
 ];
 
-class ContactList extends React.Component {
-  render() {
-    return (
-      <div>
-        <ListContacts contacts={contacts}/>
-      </div>
-    )
-  }
-}
-
 function App() {
   return (
-    <div className="App">
-      <ContactList contacts={[
-          { name: 'Tyler '},
-          { name: 'Karen' },
-          { name: 'Richard '}
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Mikenzi' },
-          { name: 'Ryan' }
-        ]}/>
-    </div>
-  );
+  <div>
+    <ListContacts contacts={contacts} />
+  </div>
+  )
 }
 
-export default App;
+export default App
